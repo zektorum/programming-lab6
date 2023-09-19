db69357b6e57f8d7d15775f38d40b757cad018b3
Murad Murtuzaliev <mur.murtuzaliev@yandex.ru>
2023-09-19 07:28:15 +0300

    fix: elements sorting

--- common-utils/src/main/java/io/github/zektorum/command/ShowCommand.java
+++ common-utils/src/main/java/io/github/zektorum/command/ShowCommand.java
@@ -3,6 +3,9 @@ package io.github.zektorum.command;
 import io.github.zektorum.data.collection.PersonStorage;
 import io.github.zektorum.data.collection.StorageAccessOwner;
 import io.github.zektorum.data.person.Person;
+import io.github.zektorum.util.Serializer;
+
+import java.lang.instrument.Instrumentation;
 
 /**
  * Реализация команды show.
@@ -15,7 +18,9 @@ public class ShowCommand extends StorageAccessOwner {
     @Override
     public String execute(CommandArgsArray args, Person person) {
         PersonStorage storage = PersonStorage.init();
+        Serializer serializer = new Serializer();
         return storage.stream()
+                .sorted((o1, o2) -> serializer.serialize(o1).length > serializer.serialize(o2).length ? 1 : 0)
                 .map(element -> storage.getElementInfo(element.getId(), element)).reduce("", String::concat);
     }
 
